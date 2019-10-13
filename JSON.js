e3451b0cae0af922bb9a64a5cd29842652d07b73
Majo14 <54584690+Majo14@users.noreply.github.com>
2019-10-13 14:25:13 -0500

    Update and rename JSON to JSON.js

---
+++ JSON.js
@@ -0,0 +1,32 @@
+//Declaracion primero objecto 
+var info =  {
+  "tipo": "PRESENTACIÓN" 
+};
+//Declaracion segundo objecto 
+var infor =  {
+  "nombre": "TACO-HEMA" 
+};
+//Declaracion tercer objecto 
+var inform =  {
+  "nombre": "MARÍA JOSÉ", 
+  "apell": " COMETA",
+  "nom": "HEIDY DAYAN",
+  "apellido": " TALERO LOZANO"
+};
+
+
+// aqui se pondra el titulo Presentacion
+var element = document.getElementById("ejem1");
+element.innerHTML= info.tipo;
+// aqui se pondra el nombre del grupo
+var elemento = document.getElementById("nombreequipo");
+elemento.innerHTML= infor.nombre;
+
+// aqui se pondra el nombre de los integrantes
+
+var elemen = document.getElementById("integran");
+elemen.innerHTML= inform.nom + inform.apellido +" - " + inform.nombre + inform.apell ;
+var ele= 
+    document.getElementById("")
+
+
